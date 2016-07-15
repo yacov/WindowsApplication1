cd7eece34f2f4557f5666b64f24ecb92b6ac9825
Iakov Volf <jakoff@gmail.com>
2016-07-15 10:04:11 +0300

    First version of encoding mthod

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,4 +1,9 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Net.Http
+Imports System.Security.Cryptography
+Imports System.Web
+
+Public Class Form1
     Private Function SSOPCG() As String
         Try
             Dim url As String = "https://dade.acceliplan.com/app/api/public/login?UserCode"
@@ -18,10 +23,10 @@
 
             'PCG is using Perl on their side so both userCode and authKey values had to be Base64-encoded to avoid errors related to character sets.
             'Also, PCG is only URL-decoding both userCode and authKey values and not the whole URL
-            data = url & HttpContext.Current.Server.UrlEncode(myAES.EncryptCustom(uc, key, 256, 128, System.Security.Cryptography.CipherMode.CBC, System.Security.Cryptography.PaddingMode.PKCS7)) _
-                    & "&AuthKey=" & HttpContext.Current.Server.UrlEncode(myAES.EncryptCustom(ak, key, 256, 128, System.Security.Cryptography.CipherMode.CBC, System.Security.Cryptography.PaddingMode.PKCS7))
-            RedirectFullURL = data
-            Return RedirectFullURL
+            data = url & HttpContext.Current.Server.UrlEncode(EncryptCustom(uc, key, 256, 128, CipherMode.CBC, System.Security.Cryptography.PaddingMode.PKCS7)) _
+                    & "&AuthKey=" & HttpContext.Current.Server.UrlEncode(EncryptCustom(ak, key, 256, 128, System.Security.Cryptography.CipherMode.CBC, System.Security.Cryptography.PaddingMode.PKCS7))
+
+            Return data
         Catch ex As Exception
             Return ex.Message
         End Try
@@ -49,4 +54,34 @@
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
 
     End Sub
+
+
+    Public Function EncryptCustom(ByVal stringToEncrypt As String, ByVal sEncryptionKey As String, keySize As Integer, blockSize As Integer, cipherMode As CipherMode, paddingMode As PaddingMode) As String
+        Dim inputByteArray(stringToEncrypt.Length) As Byte
+        Using provider = New AesCryptoServiceProvider()
+            With provider
+                .KeySize = keySize
+                .BlockSize = blockSize
+                .Mode = cipherMode
+                .Padding = paddingMode
+                .Key = System.Text.Encoding.UTF8.GetBytes(sEncryptionKey)
+            End With
+            inputByteArray = Convert.FromBase64String(stringToEncrypt)
+            Using ms = New MemoryStream(inputByteArray)
+                ' Read the first 16 bytes which is the IV.
+                Dim iv As Byte() = New Byte(15) {}
+                ms.Read(iv, 0, 16)
+                provider.IV = iv
+                Using encryptor = provider.CreateEncryptor()
+
+                    Using cs = New CryptoStream(ms, encryptor, CryptoStreamMode.Read)
+                        Using sr = New StreamReader(cs)
+                            Return sr.ReadToEnd()
+                        End Using
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Function
+
 End Class
